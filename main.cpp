34d63adb6a2fed65b38ec3e1f86b40283b969696
Jacob Wright <36747872+jacobcwright@users.noreply.github.com>
2018-10-13 10:51:15 -0600

    switched from string to 2D array for board

--- main.cpp
+++ main.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
 #include <string>
+#include <math.h>
 
 using namespace std;
 
 int main() {
 	const int DIMENSIONS = 3;
-	//char myBoard[DIMENSIONS][DIMENSIONS];
+	char myBoard[DIMENSIONS][DIMENSIONS];
 	int numPlayerOption = 2;
 	char playerOne = 'X';
 	char playerTwo = 'O';
-	string boardGraphic = "Current Board:\n|11|12|13|\n|21|22|23|\n|31|32|33|\n";
+	char initialValue = '_';
+	//string boardGraphic = "|11|12|13|\n|21|22|23|\n|31|32|33|\n";
 	int previousTurn = 1;
-	bool gameEnd = 1;
-	string placeIndex;
+	bool gameEnd = false;
+	int placeIndexRow;
+	int placeIndexColumn;
 	int placeIndexPosition;
+	int i;
+	int j;
 
 	cout << "Welcome to Tic Tac Toe!" << endl << "One or Two Players?: ";
 	cin >> numPlayerOption;
@@ -24,30 +29,72 @@ int main() {
 	if (numPlayerOption == 2) {
 		cout << "Player 1 will be " << playerOne << " 's" << endl;
 		cout << "Player 2 will be " << playerTwo << " 's" << endl;
-		cout << boardGraphic;
-
+		cout << "Current Board:" << endl;
+		for (i = 0; i < 3; i++) {
+			for (j = 0; j < 3; j++) {
+				myBoard[i][j] = initialValue;
+				cout << myBoard[i][j];
+			}
+			cout << endl;
+		}
+	
+		previousTurn = rand() % 2 + 1;
+		
 		while (!gameEnd) {
 
 			if (previousTurn == 1) {
 				cout << "It is Player 2's turn" << endl;
-				cout << "Enter position to place an \"O\": ";
-				cin >> placeIndex;
-				boardGraphic.find(placeIndex);
-				while (boardGraphic.find(placeIndex) == string::npos) {
-					cout << "Invalid option." << endl << "Enter new position: " << endl;
-					cin >> placeIndex;
-					
+				cout << "Enter row to place an \"O\": ";
+				cin >> placeIndexRow;
+				cout << endl;
+				cout << "Enter column to place an \"X\": ";
+				cin >> placeIndexColumn;
+				cout << endl;
+				while (placeIndexColumn == 0/*is not valid*/) {
+					cout << "Position not valid" << endl;
+				}
+				myBoard[placeIndexRow - 1][placeIndexColumn - 1] = playerTwo;
+				previousTurn = 2;
+				cout << "Current Board:" << endl;
+				for (i = 0; i < 3; i++) {
+					for (j = 0; j < 3; j++) {
+						cout << myBoard[i][j];
+					}
+					cout << endl;
 				}
 			}
 			else if (previousTurn == 2) {
-
+				cout << "It is Player 2's turn" << endl;
+				cout << "Enter row to place an \"O\": ";
+				cin >> placeIndexRow;
+				cout << endl;
+				cout << "Enter column to place an \"X\": ";
+				cin >> placeIndexColumn;
+				cout << endl;
+				while (placeIndexColumn == 0/*is not valid*/) {
+					cout << "Position not valid" << endl;
+				}
+				myBoard[placeIndexRow - 1][placeIndexColumn - 1] = playerTwo;
+				previousTurn = 2;
+				cout << "Current Board:" << endl;
+				for (i = 0; i < 3; i++) {
+					for (j = 0; j < 3; j++) {
+						cout << myBoard[i][j];
+					}
+					cout << endl;
+				}
 			}
+			/*for (i = 0; i < boardGraphic.size(); i++) {
+				if (false) {
+					gameEnd = true;
+				}
+			}*/
+			
+
+
 		}
 	}
-	else {
-		cout << "Invalid option stupid." << endl;
-	}
-
+	
 	system("pause");
 	return 0;
 }
